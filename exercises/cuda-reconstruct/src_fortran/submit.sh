705b724ac822c8612cad73be580fae158332175e
Rupert Nash <r.nash@epcc.ed.ac.uk>
2020-01-07 14:42:55 +0000

    import

---
+++ exercises/cuda-reconstruct/src_fortran/submit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+#PBS -N cuda_reconstruct
+#PBS -q gpu-teach
+#PBS -l select=1:ncpus=10:ngpus=1
+#PBS -l walltime=0:01:00
+
+# Budget: use either your default or the reservation
+#PBS -A d171
+
+# Load the required modules
+module load gcc cuda pgi
+
+# Pick a random device as PBS on Cirrus not yet configured to control
+# GPU visibility
+r=$RANDOM; let "r %= 4";
+export CUDA_VISIBLE_DEVICES=$r
+
+cd $PBS_O_WORKDIR
+
+./reconstruct
